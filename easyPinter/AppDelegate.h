f9e56a508b786720feb2c8a4554434d8497d51e7
AdemJensen <rentenglong@163.com>
2021-05-06 16:23:05 +0800

    Initial Commit

---
+++ easyPinter/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  easyPinter
+//
+//  Created by 伊米哇嘎克 on 2021/5/6.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ easyPinter/main.m
@@ -0,0 +1,15 @@
+//
+//  main.m
+//  easyPinter
+//
+//  Created by 伊米哇嘎克 on 2021/5/6.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // Setup code that might create autoreleased objects goes here.
+    }
+    return NSApplicationMain(argc, argv);
+}
